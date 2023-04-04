1273ed7a751833d11195c08630b937122daa4193
iCMANTIs <cwenyu1@gmail.com>
2023-04-04 10:41:42 -0600

    UI&HouseBroken

---
+++ Assets/Scripts/BuildingBreak.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingBreak : MonoBehaviour
+{
+    public float DelayDeleteCollider = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DeleteCollider()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("碰到的物体：" + collision.gameObject.tag);
+        if(collision.gameObject.CompareTag("Excavator"))
+        {
+            print("碰到的物体：播放动画");
+            Invoke("DeleteCollider", DelayDeleteCollider);
+            GetComponent<Animator>().enabled = true;
+        }
+    }
+}
